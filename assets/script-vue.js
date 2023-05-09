4d11f86c6f7f43796fe0ca68786a5be9fe8dbc46
Paolo96700 <paolo.falcoapp@gmail.com>
2023-05-09 17:55:55 +0200

    Correzioni

--- assets/script-vue.js
+++ assets/script-vue.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            newPoint: {text:''},
+            newPoint: {
+                text:'',
+                done: false
+            },
             ListArray: [
                 {
                     text:'fare la spesa',
@@ -20,17 +23,20 @@ const app = Vue.createApp({
         }
     },
     methods: {
-            addTodo() {
-                if (this.newPoint.trim()) {
-                this.ListArray.unshift({ text: this.newPoint, done: false })
-                this.newTodo = ''
-                }
-
-            deleteItem(index) {
-                this.ListArray[index].done = !this.ListArray[index].done;
-            }
+        addPoint() {
+          if (this.newPoint.trim()) {
+            this.ListArray.unshift({ text: this.newPoint, done: false })
+            this.newPoint = ''
+          }
         },
+        remove(index) {
+          this.ListArray.splice(index, 1)
+        },
+        toggleDone(index) {
+          this.ListArray[index].done = !this.ListArray[index].done
+        }
     },
+
 });
 
 app.mount('#todo');
