e5ed3186ad65e951ab7ced769487fae3aa578abb
Paolo96700 <paolo.falcoapp@gmail.com>
2023-05-09 17:53:54 +0200

    es

--- assets/script-vue.js
+++ assets/script-vue.js
@@ -1,7 +1,36 @@
 const app = Vue.createApp({
+    data() {
+        return {
+            newPoint: {text:''},
+            ListArray: [
+                {
+                    text:'fare la spesa',
+                    done: false
+                },
+                {
+                    text:'pulire casa',
+                    done: false
+                },
+                {
+                    text:'riparare la macchina',
+                    done:false
+                }
+               
+            ]  
+        }
+    },
+    methods: {
+            addTodo() {
+                if (this.newPoint.trim()) {
+                this.ListArray.unshift({ text: this.newPoint, done: false })
+                this.newTodo = ''
+                }
 
-
-
+            deleteItem(index) {
+                this.ListArray[index].done = !this.ListArray[index].done;
+            }
+        },
+    },
 });
 
-app.mount('#todo')
+app.mount('#todo');
